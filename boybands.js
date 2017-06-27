e9f80f5a224cdf4d6917b476a2cd7b7f409c9aa2
jevonthomas <jevon.g.thomas@gmail.com>
2017-06-27 12:34:53 -0500

    able to print each array to the DOM

---
+++ boybands.js
@@ -0,0 +1,36 @@
+var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+
+// The number of loops to perform (what if the array changes?)
+var bandLoopCount = bands.length;
+var veggieLoopCount = vegetables.length;
+
+// Keep track of which band we're on in the loop
+var currentBand = "";
+
+// Keep track of which veggie we're on in the loop
+var currentVeggie = "";
+
+// Get a reference to the appropriate DOM element for bands
+var bandElement = document.getElementById("boy-bands");
+
+// Get a reference to the appropriate DOM element for vegetables
+var veggieElement = document.getElementById("vegetables");
+
+// Start looping
+for (var i = 0; i < bandLoopCount; i++) {
+
+  // Add the band names into the correct <div>
+  currentBand += bands[i] + " ";
+
+  bandElement.innerHTML = currentBand;
+
+}
+
+for (var i = 0; i < veggieLoopCount; i++) {
+  // Add the veggie names into the correct <div>
+  currentVeggie += vegetables[i] + " ";
+
+  veggieElement.innerHTML = currentVeggie;
+
+}
